5e60cb2a1f6d7fcfcfef2644d4613ff1444a8d25
Rashed Arman <rashedarman21@gmail.com>
2023-02-01 00:36:59 +0600

    Feat: remove book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const dispatch = useDispatch();
+
+  const { id, title, author } = props;
 
   return (
     <li>
@@ -11,11 +15,15 @@ const Book = (props) => {
         {' by '}
         {author}
       </p>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>
+        remove
+      </button>
     </li>
   );
 };
 
 Book.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
