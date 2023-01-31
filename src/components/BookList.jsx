8cb2343c82c2f5248a1fb2891d691565383ba513
Rashed Arman <rashedarman21@gmail.com>
2023-02-01 00:30:07 +0600

    Add BookList prop validation

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,31 +1,24 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Book from './Book';
 
-const BookListData = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First century',
-    author: 'Suzanne Collins',
-  },
-];
-
-const BookList = () => (
+const BookList = ({ books }) => (
   <ul>
-    {BookListData.map((book) => {
+    {books.map((book) => {
       const { id, title, author } = book;
-      return <Book key={id} title={title} author={author} />;
+      return <Book key={id} id={id} title={title} author={author} />;
     })}
   </ul>
 );
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default BookList;
