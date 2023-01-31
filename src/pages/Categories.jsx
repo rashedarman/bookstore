c966e40f455cf15439e1b9758519ab8765adef90
Rashed Arman <rashedarman21@gmail.com>
2023-02-01 00:39:49 +0600

    Feat: check status

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => <button type="button">Check status</button>;
+const Categories = () => {
+  const dispatch = useDispatch();
+
+  const categories = useSelector((state) => state.categories);
+
+  return (
+    <>
+      <p>{categories.status}</p>
+      <button type="button" onClick={() => dispatch(checkStatus())}>
+        Check status
+      </button>
+    </>
+  );
+};
 
 export default Categories;
