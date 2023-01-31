2f08d57742190069e55d9654a8f0edcbd186d592
Rashed Arman <rashedarman21@gmail.com>
2023-02-01 00:38:42 +0600

    Show books on the page

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from 'react-redux';
 import BookForm from '../components/BookForm';
 import BookList from '../components/BookList';
 
-const BooksPage = () => (
-  <>
-    <BookList />
-    <br />
-    <BookForm />
-  </>
-);
+const BooksPage = () => {
+  const { books } = useSelector((state) => state.books);
+
+  return (
+    <>
+      <BookList books={books} />
+      <br />
+      <BookForm />
+    </>
+  );
+};
 
 export default BooksPage;
