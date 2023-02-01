69819b16f1b7f5f2af195395567b7ca59d830b69
Rashed Arman <rashedarman21@gmail.com>
2023-02-02 04:50:34 +0600

    Add redux-thunk middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,9 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  applyMiddleware,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import books from './books/books';
 import categories from './categories/categories';
 
@@ -6,6 +11,7 @@ const rootReducer = combineReducers({ books, categories });
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: applyMiddleware(thunk),
 });
 
 export default store;
