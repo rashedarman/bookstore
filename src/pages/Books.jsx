9e47d9f824c9218e785e5bf6f96daf79baeea92a
Rashed Arman <rashedarman21@gmail.com>
2023-02-02 06:36:59 +0600

    Fetch API books on initial page load

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,10 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BookForm from '../components/BookForm';
 import BookList from '../components/BookList';
+import { fetchBooks } from '../redux/books/books';
 
 const BooksPage = () => {
-  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  const { books } = useSelector((state) => state.books);
   return (
     <>
       <BookList books={books} />
